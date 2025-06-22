61da8a9a6f0dd196d6e9321900dc35972e88d60c
m <m>
2025-06-22 00:04:54 -0300

    Correcciones y agregado de clases

---
+++ Files/cheapInterpreter.cs
@@ -0,0 +1,12 @@
+namespace Coding
+{
+    /// <summary>
+    /// Interprete encargado de analizar codigo escrito en el lenguaje
+    /// de no tan bajo nivel para poder convertirlo luego en binarios para 
+    /// el emulador
+    /// </summary>
+    class CheapInterpreter
+    {
+
+    }
+}
